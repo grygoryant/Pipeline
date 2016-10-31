9b1949948b00148b42d40ac79bb7b3a067356f69
Grigorii M. Antiokh <antioh@pkcc.ru>
2016-10-31 13:07:38 +0300

    Move removed from inout stages

--- main.cpp
+++ main.cpp
@@ -16,16 +16,12 @@
 #include "timer.hpp"
 
 const int IN_PACKAGE_SIZE = 1024*768;
-const int IN_PACKAGE_NUM = 10000;
+const int IN_PACKAGE_NUM = 100;
 
 class ConcreteInStage : public InStage {
-protected:
-	
 public:
-
 	void StageTask( ) override {
 		for( int i = 0; i < IN_PACKAGE_NUM; ++i ) {
-			
 			auto msg = std::make_unique<Message>( i, IN_PACKAGE_SIZE,
 												 ( i == IN_PACKAGE_NUM - 1 ) ? MessageType::last : MessageType::notLast );
 			//std::cout << "Push it to the limit " << i << "\n";
@@ -45,25 +41,21 @@ public:
 
 class FirstConcreteInOutStage : public InOutStage {
 public:
-	FirstConcreteInOutStage() : InOutStage() { }
-	
 	std::unique_ptr<Message> StageTask( std::unique_ptr<Message> msg ) override {
 		std::for_each( std::begin( msg->data ) , std::end( msg->data ), [] ( int &n ) {
 			n *= 10;
 		});
-		return std::move( msg );
+		return msg;
 	}
 };
 		
 class SecondConcreteInOutStage : public InOutStage {
 public:
-	SecondConcreteInOutStage() : InOutStage() { }
-	
 	std::unique_ptr<Message> StageTask( std::unique_ptr<Message> msg ) override {
 		std::for_each( std::begin( msg->data ) , std::end( msg->data ), [] ( int &n ) {
 			n /= 10;
 		});
-		return std::move( msg );
+		return msg;
 	}
 };
 
@@ -97,24 +89,24 @@ int main(int argc, const char * argv[]) {
 	
 	std::cout << "Elapsed time: " << elapsedTime( t1, t2 ) << "\n";
 	
-	t1 = currentTime();
-	for( int i = 0; i < IN_PACKAGE_NUM; ++i ) {
-		std::vector<int> v( IN_PACKAGE_SIZE, i );
-		std::for_each( std::begin( v ) , std::end( v ), [] ( int &n ) {
-			n *= 10;
-			n /= 10;
-		});
-		std::for_each( std::begin( v ) , std::end( v ), [] ( int &n ) {
-			n *= 10;
-			n /= 10;
-		});
-		std::string m( "Read " );
-		m += std::to_string( v[0] ) + "\n";
-		std::cerr << m;
-	}
-	t2 = currentTime();
-	
-	std::cout << "Elapsed time: " << elapsedTime( t1, t2 ) << "\n";
+//	t1 = currentTime();
+//	for( int i = 0; i < IN_PACKAGE_NUM; ++i ) {
+//		std::vector<int> v( IN_PACKAGE_SIZE, i );
+//		std::for_each( std::begin( v ) , std::end( v ), [] ( int &n ) {
+//			n *= 10;
+//			n /= 10;
+//		});
+//		std::for_each( std::begin( v ) , std::end( v ), [] ( int &n ) {
+//			n *= 10;
+//			n /= 10;
+//		});
+//		std::string m( "Read " );
+//		m += std::to_string( v[0] ) + "\n";
+//		std::cerr << m;
+//	}
+//	t2 = currentTime();
+//	
+//	std::cout << "Elapsed time: " << elapsedTime( t1, t2 ) << "\n";
 	
 	return 0;
 }
